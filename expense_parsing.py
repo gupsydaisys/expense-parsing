1805a170f940f84bfc08c49ee40df7dbb42a6806
Serena Gupta <serena.gupta@grandrounds.com>
2018-05-21 22:35:56 -0700

    [(master)] Update expense_parsing code

--- expense_parsing.py
+++ expense_parsing.py
@@ -2,50 +2,46 @@ from openpyxl import load_workbook
 
 from sets import Set
 
-# TODO CONFIGURE: Must set to be located in the same folder as this file
-XLSX_NAME = 'Expenses.xlsx'
-
-# Workbook that has all expenses in it
-WB = load_workbook(XLSX_NAME)
-
 #######################################################################################################################
 ################################################### STEP 1 !!!!!!!  ####################################################
-# 1. Configure TODO
+# 1. Look for TODO
 #######################################################################################################################
 
-# TODO CONFIGURE: Must set to be all the sheet names you want to skip
-SKIPPED_SHEETS = ['Totals', 'Before-Work', 'LabelMap']
+# TODO CONFIGURE: Set to name of sheet that references other sheets, must be the same directory as this program
+INDEX_SHEET_NAME = 'Index.xlsx'
+INDEX_WB = load_workbook(INDEX_SHEET_NAME)
 
-def sheet_date_names():
-  arr = WB.get_sheet_names()
-  for sheet_name in SKIPPED_SHEETS:
-    arr.remove(sheet_name)
-  return arr
+# TODO CONFIGURE: Set to name of Sheet holding a week worth of expenses
+E0 = 'Expenses0.xlsx'
+E1 = 'Expenses1.xlsx'
+E0_WB = load_workbook(E0)
+E1_WB = load_workbook(E1)
 
-# [Array] an array of sheet names that have item names and costs on them
-SHEET_DATE_NAMES_ARRAY = sheet_date_names()
+# TODO Configure: If you added a workbook, add it this array as well
+EXPENSE_WBS = [E0_WB, E1_WB]
 
 # A set of every item name that is entered in as an expense
 all_item_names = Set()
 
 # @return a list of all unique item names across all sheets
 def get_all_unique_item_names():
-  for sheet_name in SHEET_DATE_NAMES_ARRAY:
-    add_item_names(WB[sheet_name])
+  for wb in EXPENSE_WBS:
+    for sheet_name in wb.sheetnames:
+      add_item_names(wb[sheet_name])
   return all_item_names
 
 def add_item_names(sheet):
   for col_row in sheet['B']:
+    if col_row.value is None:
+      continue
     all_item_names.add(col_row.value.strip())
   return True
 
 #######################################################################################################################
 ################################################### STEP 2 !!!!!!!  ####################################################
-# 1. Configure TODO
+# 1. lOOK FOR TODO
+# Note: The easiest way to see items not mapped to is by running `map_label_to_row_on_sheet()` and looking at the errors (line 103)
 #######################################################################################################################
-
-# TODO CONFIGURE: Must be set to the index of the last row with an item name and label pair on sheet LabelMap
-LABEL_MAP_LAST_ROW_INDEX = 182
 
 # This is a list of all labels that we use to label a type of expense.  Note that the actual label may not be in this
 # set however every actual label should map to something in this set.
@@ -54,8 +50,10 @@ all_possible_item_labels = Set()
 # @return a map of each item name to a label
 def map_item_names_to_label():
   label_map = {}
-  label_map_sheet = WB['LabelMap']
-  for index in range(1, LABEL_MAP_LAST_ROW_INDEX + 1):
+  label_map_sheet = INDEX_WB['LabelMap']
+  for index in range(1, 500):
+    if label_map_sheet[index][0].value is None:
+      continue
     item_name = label_map_sheet[index][0].value.strip()
     label = label_map_sheet[index][1].value
     label_map[item_name] = label
@@ -65,11 +63,14 @@ def map_item_names_to_label():
 # Map of every time name to a label
 LABEL_MAP = map_item_names_to_label()
 
+# TODO: Uncomment out the lines below to see what new mappings you need to add to LabelMap
+# all_item_names = set(get_all_unique_item_names())
+# all_mapped_item_names = set(LABEL_MAP.values() + LABEL_MAP.keys())
+# new_item_names_to_add_to_map = all_item_names - all_mapped_item_names
 
 #######################################################################################################################
 ################################################### STEP 3 !!!!!!!  ####################################################
-# 1. Configure TODO
-# 2. Run cost_highest_to_lowest
+# 1. Look for TODO
 #######################################################################################################################
 
 # @return a map where the keys are a label and value are an array of row data
@@ -84,28 +85,31 @@ LABEL_MAP = map_item_names_to_label()
 #          ]
 def map_label_to_row_on_sheet():
   cray_zay_map = {}
-  for sheet_name in SHEET_DATE_NAMES_ARRAY:
-    sheet = WB[sheet_name]
-    last_index = 100
-    for index in range(1, last_index):
-      item_name = sheet[index][1].value
-      if item_name is None:
-        continue
-      key_label
-      if item_name.strip() in LABEL_MAP:
-        key_label = LABEL_MAP[item_name.strip()]
-      elif item_name.strip() in all_possible_item_labels:
-        key_label = item_name.strip()
-      else:
-        print "Error!!!! The following item was not mapped to a label: " + item_name
-        print "Make sure you add that item_name to LabelMaps in Google Sheets"
-      cost = sheet[index][2].value
-      if (key_label not in cray_zay_map):
-        cray_zay_map[key_label] = []
-      cray_zay_map[key_label].append({'name': item_name, 'cost': cost, 'sheet': sheet_name })
+  for wb in EXPENSE_WBS:
+    for sheet_name in wb.sheetnames:
+      sheet = wb[sheet_name]
+      last_index = 100
+      for index in range(1, last_index):
+        item_name = sheet[index][1].value
+        if item_name is None:
+          continue
+        key_label = ''
+        if item_name.strip() in LABEL_MAP:
+          key_label = LABEL_MAP[item_name.strip()]
+        elif item_name.strip() in all_possible_item_labels:
+          key_label = item_name.strip()
+        else:
+          # print item_name
+          print "Error!!!! The following item was not mapped to a label: " + item_name
+          # print "Make sure you add that item_name to LabelMaps in Google Sheets"
+        cost = sheet[index][2].value
+        if (key_label not in cray_zay_map):
+          cray_zay_map[key_label] = []
+        cray_zay_map[key_label].append({'name': item_name, 'cost': cost, 'sheet': sheet_name })
   return cray_zay_map
 
-MAP_OF_LABELS_TO_ITEM_ROWS = map_label_to_row_on_sheet()
+# TODO uncomment out the below line
+# MAP_OF_LABELS_TO_ITEM_ROWS = map_label_to_row_on_sheet()
 
 def calculate_costs_per_label():
   mappy = {}
@@ -114,9 +118,20 @@ def calculate_costs_per_label():
     mappy[key] = yolo
   return mappy
 
-MEOW = calculate_costs_per_label()
+# TODO uncomment out the below line
+# MEOW = calculate_costs_per_label()
 
 print "TODO: Check for negatives and go through ? and categorize"
 
 def cost_highest_to_lowest():
   return sorted(MEOW, key=MEOW.get, reverse=True)
+
+# TODO: Uncomment these out get printed a list of items highest spend to lowest spend
+# and then a list of the cost associated with each
+
+# costs_h_to_l = cost_highest_to_lowest()
+# for x in costs_h_to_l:
+  # print x
+
+# for x in costs_h_to_l:
+  # print MEOW[x]
